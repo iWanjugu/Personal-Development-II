2155336e87c2b26deb6bd392f88a74c93f47061f
Wanjugu Irene <ireneemaina@gmail.com>
2015-11-04 12:00:13 +0300

    Insert Comma: Not complete

---
+++ int_comma.py
@@ -0,0 +1,31 @@
+__author__ = 'iWanjugu'
+# FORMAT STRING
+# Using PYTHON make a program that can format any number to add a comma to its appropriate position
+# For example (1000000) becomes (1,000,000)
+
+num = 1000
+num_ = str(num)
+
+print(num_)
+print(num_.split(""))
+print(num_[-4])
+
+
+
+# comma = []
+# for i in num_:
+#     comma = comma.append(num_[-i])
+#     print(comma)
+#     # if (i%3 == 0):
+
+
+
+# for i in num_:
+#     print(num_[i])
+
+
+#
+# h = (i for i, x in enumerate(num_) if x==0)
+#
+# for i in h:
+# 	print (num_[i]);
