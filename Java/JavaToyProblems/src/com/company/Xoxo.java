a3e83c21aa8d7ff8269ae91eb675a1aa7bcefab9
Wanjugu Irene <ireneemaina@gmail.com>
2015-11-25 20:47:12 +0300

    XOXO. java - counting whether x's and o's are equal - NOTcomplete

---
+++ Java/JavaToyProblems/src/com/company/Xoxo.java
@@ -0,0 +1,24 @@
+package com.company;
+
+/**
+ * Created by iWanjugu on 25/11/2015.
+ */
+public class Xoxo {
+    public static void main (String [] args) {
+        String s = "xoxoxoxx";
+        String [] str = s.split("");
+        System.out.println(str);
+
+        int a = 0;
+        int b = 0;
+
+//        int i = 0;
+//        for (i = 1; str.size() == i; i++) {
+//
+//            if (str[i] == "x") {
+//                a += 1
+//
+//            }
+//        }
+    }
+}
